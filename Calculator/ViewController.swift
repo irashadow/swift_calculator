c79dd72c259634dc3370ed4ff76d7ab2048df404
irashadow <irashadow@gmail.com>
2016-09-01 16:10:31 +0800

    add ViewController function of Float Button

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -13,26 +13,39 @@ class ViewController: UIViewController {
     @IBOutlet private weak var display: UILabel!
     
     
-    private var userIsInTheMiddleOfTyping = false
+    private var userIsInTheMiddleOfTyping = false {
+        didSet {
+            if !userIsInTheMiddleOfTyping {
+                userIsInTheMiddleOfFloatingPointNummer = false
+            }
+        }
+    }
     
-    //
     private var userIsInTheMiddleOfFloatingPointNummer = false
     
-
+    
     
     
     @IBAction private func touchDigit(sender: UIButton) {
-        let digit = sender.currentTitle!
+        var digit = sender.currentTitle!
+        
+        if digit == "." {
+            if userIsInTheMiddleOfFloatingPointNummer {
+                return
+            }
+            if !userIsInTheMiddleOfTyping {
+                digit = "0."
+            }
+            userIsInTheMiddleOfFloatingPointNummer = true
+        }
         
         if userIsInTheMiddleOfTyping{
-            
             let textCurrentlyInDisplay = display.text!
             display.text = textCurrentlyInDisplay + digit
         }else{
             display.text = digit
+            userIsInTheMiddleOfTyping = true
         }
-        userIsInTheMiddleOfTyping = true
-        //print("touched \(digit) Digit")
     }
     
     private var displayValue: Double {

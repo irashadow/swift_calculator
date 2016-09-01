3a2c3426ce75d555bce7fe8d7f623bf61d1866d3
irashadow <irashadow@gmail.com>
2016-09-01 15:51:19 +0800

    first commit

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,17 +9,59 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet private weak var display: UILabel!
+    
+    
+    private var userIsInTheMiddleOfTyping = false
+    
+    //
+    private var userIsInTheMiddleOfFloatingPointNummer = false
+    
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    @IBAction private func touchDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        
+        if userIsInTheMiddleOfTyping{
+            
+            let textCurrentlyInDisplay = display.text!
+            display.text = textCurrentlyInDisplay + digit
+        }else{
+            display.text = digit
+        }
+        userIsInTheMiddleOfTyping = true
+        //print("touched \(digit) Digit")
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    private var displayValue: Double {
+        get{
+            return Double(display.text!)!
+        }
+        
+        set {
+            display.text = String(newValue)
+        }
     }
-
-
+    
+    
+    private var brain = CalculatorBrain()
+    @IBAction private func performOperation(sender: UIButton) {
+        
+        if userIsInTheMiddleOfTyping{
+            brain.setOperand(displayValue)
+            userIsInTheMiddleOfTyping = false
+        }
+        
+        if let mathmaticalSymbol = sender.currentTitle{
+            brain.performOperation(mathmaticalSymbol)
+        }
+        
+        displayValue = brain.result
+        
+    }
+    
+    
 }
 
